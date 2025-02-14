b1fc5e11e7a344d4a97df04e6ba1c9addb08f4de
WahyuDin Ambia XD <131398263+W4hyuXD@users.noreply.github.com>
2025-02-14 14:36:39 +0700

    Delete PenangananForm directory

--- PenangananForm/proses02.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_POST['Input'])) {
-  $nama = $_POST['nama'];
-  echo "Nama Anda : <b>$nama</b>";
-}
-?>
--- PenangananForm/proses03.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_GET['Input'])) {
-  $nama = $_GET['nama'];
-  echo "Nama Anda : <b>$nama</b>";
-}
-?>
