36ff1d8240aefa9142f9e85949c04e11c34f1c25
WahyuDin Ambia XD <131398263+W4hyuXD@users.noreply.github.com>
2025-02-14 14:39:23 +0700

    Delete StrukturKondisi directory

--- StrukturKondisi/if_else2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = "WahyuXD";
-$pass = "111";
-if ($user == "WahyuXD" && $pass == "111") {
-  echo "Login Berhasil";
-} else {
-  echo "Login Gagal";
-}
-?>
--- StrukturKondisi/if_var.php
+++
@@ -1,8 +0,0 @@
-<?php
-$user = "";
-if (!isset($user)) {
-  echo "Variabel tidak ada/belum terbentuk";
-} else {
-  echo "Variabel ada";
-}
-?>
