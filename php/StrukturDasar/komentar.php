b6d47239bb59278b7a3b61470b8fcca4ddc5634d
WahyuDin Ambia XD <131398263+W4hyuXD@users.noreply.github.com>
2025-02-14 14:37:56 +0700

    Add files via upload

---
+++ php/StrukturDasar/komentar.php
@@ -0,0 +1,10 @@
+<?
+/* Komentar untuk membungkus beberapa baris */ 
+/*
+echo "Hello, Woeld!";
+*/
+
+// Komentar Untuk Satu Baris (//)
+# Komentar v3 
+echo "Halo, Selamat Siang!";
+?>
---
+++ php/StrukturDasar/konstanta.php
@@ -0,0 +1,8 @@
+<?php
+define ("NAME", "WahyuDin Ambia");
+define ("MARK", 90);
+
+// Jika Name = "WahyuDin Ambia"; ( Akan Menyebabkan error jika menggunakan variabel )
+echo "Nama : " . NAME;
+echo "<br> Nilai : " . MARK;
+?>
